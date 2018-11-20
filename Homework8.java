fa7232900ac33c2a6117ead19f90a4bcf9aa0691
Rudolph <rudoljus@berkeleyprep.org>
2018-11-19 21:31:25 -0500

    Justin Rudolph HW 8

--- Homework8.java
+++ Homework8.java
@@ -4,34 +4,46 @@ public class Homework8 {
 	 * variables. A Homework8 object should have an array that
 	 * holds n int values
 	 */
-	public Homework8(int n) {
+	 private int[] arr;
 
+	public Homework8(int n) {
+		arr = new int[n];
 	}
 
 	/* Return the stored array
 	 */
 	public int[] problem1() {
-
+		return arr;
 	}
 
 	/* Fill the stored array with the provided int
 	 */
 	public void problem2(int n) {
-
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = n;
+		}
 	}
 
 	/* Fill the stored array. The first value should be
 	 * n, the second n-1, the third n-2, etc.
 	 */
 	public void problem3(int n) {
-
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = n - i;
+		}
 	}
 
 	/* Find and return the maximum value in the
 	 * provided array
 	 */
 	public double problem4(double[] arr) {
-
+		double max = arr[0];
+		for(double i: arr) {
+			if(i > max) {
+				max = i;
+			}
+		}
+		return max;
 	}
 
 	/* Find and return the index of num in the
@@ -39,8 +51,14 @@ public class Homework8 {
 	 * is not in the array
 	 */
 	public int problem5(int[] arr, int num) {
-
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return arr[i];
+			}
+		}
+		return -1;
 	}
+
 
 	public static void main(String[] args) {
 		boolean pass = true;
